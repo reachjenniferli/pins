a710645bbf659ffdb2fd4dc7632af74229e177dd
Jennifer Li <jenniferli7437@gmail.com>
2025-06-17 09:01:24 -0400

    try wrapping in DOM

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-// Select all pins
-const pins = document.querySelectorAll('.pin');
+document.addEventListener('DOMContentLoaded', () => {
+    // Select all pins
+    const pins = document.querySelectorAll('.pin');
 
-pins.forEach(pin => {
-  pin.onmousedown = function(event) {
-    const dragged = event.currentTarget;
+    pins.forEach(pin => {
+    pin.onmousedown = function(event) {
+        const dragged = event.currentTarget;
 
-    dragged.style.position = 'absolute';
-    dragged.style.zIndex = 1000;
+        dragged.style.position = 'absolute';
+        dragged.style.zIndex = 1000;
 
-    // Move dragged element directly to body to position relative to page
-    document.body.append(dragged);
+        // Move dragged element directly to body to position relative to page
+        document.body.append(dragged);
 
-    function moveAt(pageX, pageY) {
-      dragged.style.left = pageX - dragged.offsetWidth / 2 + 'px';
-      dragged.style.top = pageY - dragged.offsetHeight / 2 + 'px';
-    }
+        function moveAt(pageX, pageY) {
+        dragged.style.left = pageX - dragged.offsetWidth / 2 + 'px';
+        dragged.style.top = pageY - dragged.offsetHeight / 2 + 'px';
+        }
 
-    moveAt(event.pageX, event.pageY);
+        moveAt(event.pageX, event.pageY);
 
-    function onMouseMove(event) {
-      moveAt(event.pageX, event.pageY);
-    }
+        function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY);
+        }
 
-    document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mousemove', onMouseMove);
 
-    dragged.onmouseup = function() {
-      document.removeEventListener('mousemove', onMouseMove);
-      dragged.onmouseup = null;
+        dragged.onmouseup = function() {
+        document.removeEventListener('mousemove', onMouseMove);
+        dragged.onmouseup = null;
+        };
+
+        event.preventDefault();
     };
-
-    event.preventDefault();
-  };
+    });
 });

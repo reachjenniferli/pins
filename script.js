4d6efbfa12d251ca6fc6b79a2c826fd4bdafa96f
Jennifer Li <jenniferli7437@gmail.com>
2025-06-18 13:29:23 -0400

    add flip prototype

--- script.js
+++ script.js
@@ -6,37 +6,38 @@ document.addEventListener('DOMContentLoaded', () => {
     pin.onmousedown = function(event) {
         const dragged = event.currentTarget;
 
+        const offsetX = event.clientX - dragged.getBoundingClientRect().left;
+        const offsetY = event.clientY - dragged.getBoundingClientRect().top;
+
         dragged.style.position = 'absolute';
         dragged.style.zIndex = 1000;
 
         // Move dragged element directly to body to position relative to page
         document.body.append(dragged);
 
+        dragged.classList.add('dragging')
+
         function moveAt(pageX, pageY) {
-        dragged.style.left = pageX - dragged.offsetWidth / 2 + 'px';
-        dragged.style.top = pageY - dragged.offsetHeight / 2 + 'px';
+        dragged.style.left = pageX - offsetX + 'px';
+        dragged.style.top = pageY - offsetY + 'px';
         }
 
         moveAt(event.pageX, event.pageY);
 
         function onMouseMove(event) {
-        moveAt(event.pageX, event.pageY);
+            moveAt(event.pageX, event.pageY);
         }
 
         document.addEventListener('mousemove', onMouseMove);
 
         dragged.onmouseup = function() {
-        document.removeEventListener('mousemove', onMouseMove);
-        dragged.onmouseup = null;
+            document.removeEventListener('mousemove', onMouseMove);
+            dragged.onmouseup = null;
+
+            dragged.classList.remove('dragging')
         };
 
         event.preventDefault();
-    };
-    });
-
-    pins.forEach(pin => {
-    pin.onmouseover = function(event) {
-        
     };
     });
 });
